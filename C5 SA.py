2b5065fb801169a1bdcc05c75c77c3c722727c33
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-10-19 12:51:02 +0530

    Update C5 SA.py

--- C5 SA.py
+++ C5 SA.py
@@ -4,6 +4,9 @@ pygame.init()
 
 screen = pygame.display.set_mode((1200, 400))
 
+# Declare score with a value 0
+score_font = pygame.----.----("freesansbold.ttf", 16)  # Replace the '--' to complete the line
+
 dino_rect = pygame.Rect(100, 250, 64, 64)
 cactus_rect = pygame.Rect(1100, 300, 32, 32)
 ground_rect = pygame.Rect(0, 330, 1200, 2)
@@ -15,7 +18,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
             
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -33,10 +35,19 @@ while True:
     cactus_rect.x = cactus_rect.x - 1
     if cactus_rect.x <= -30:
         cactus_rect.x = 1200
-        
+    
+    score -- 1                         # Increment score by 1 by replacing '+=' and putting in correct operator.
+    show_score = round(score/---)      # Replace '--' with a number to reduce the speed
+    score_show = score_font.------("Score: " + str(show_score), True, (0, 0, 0))    # Replace the '--' to complete the line
+    screen.----(----------, (10, 10))         # Replace the '--' to complete the line
+    
     pygame.draw.rect(screen, (100, 100, 100), dino_rect)
     pygame.draw.rect(screen, (100, 100, 100), cactus_rect)
     pygame.draw.rect(screen, (100, 100, 100), ground_rect)
+    
+    if dino_rect.colliderect(cactus_rect):
+        pygame.time.delay(2000)
+        pygame.quit()
     
     pygame.display.update()
     
